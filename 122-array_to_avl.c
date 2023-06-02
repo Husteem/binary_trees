4d2e5c3950d264c755df5a95721a67e6d9856059
pacmanlolo <michael.omotosho7@gmail.com>
2023-06-02 03:32:28 -0700

    AVL - Array to AVL

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -7,7 +7,6 @@
  * Return: pointer to the root node of the created AVL tree,
  * or NULL on failure. If a value of the array is already present in the
  * tree, this value must be ignored
- *avl
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
